ce5ab2457033560917b6fc8e8036ed15cd65d6c6
Hritabhash Ray <118247988+Hritabhash1@users.noreply.github.com>
2025-07-18 14:49:19 +0530

    Delete kabitalok-payments directory

--- kabitalok-payments/src/utils/db.js
+++
@@ -1,9 +0,0 @@
-import Dexie from 'dexie';
-
-export const db = new Dexie('KabitalokDB');
-
-db.version(4).stores({
-  students: '++id,studentId,name,contact,course,admissionDate,email',
-  payments: '++id,studentId,amount,date,note,collector',
-  expenditures: '++id,date,amount,reason,person'
-});
